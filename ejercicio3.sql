109bf7d27da70c72727d2e1f532e5e5af23fea6b
Lucas Dominguez <lucasdomin62@gmail.com>
2023-09-07 09:16:36 -0300

    Ejercicio3 update hecho

---
+++ ejercicio3.sql
@@ -0,0 +1,11 @@
+--Ejercicio 3:
+UPDATE user  SET
+username = "Nikolakito",
+password = "Hola"
+WHERE id == 1;
+UPDATE post SET
+title = "Ya no busco novia gracias";
+update post SET
+body = "se llama ambar"
+WHERE author_id == 1;
+--Update de usuario y/o post
