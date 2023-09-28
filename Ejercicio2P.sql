82384a980e18dbc1e063ef32e1c06c51d53f6208
Lucas Dominguez <lucasdomin62@gmail.com>
2023-09-28 11:13:06 -0300

    ejercicio 2 y 3 postgres

---
+++ Ejercicio2P.sql
@@ -0,0 +1,10 @@
+--Ejercicio 2 
+UPDATE "user"  SET
+username = 'Nikolakito',
+password = 'Hola'
+WHERE id = 1;
+UPDATE "post" SET
+title = 'Ya no busco novia gracias';
+update "post" SET
+body = 'se llama ambar'
+WHERE author_id = 1;
